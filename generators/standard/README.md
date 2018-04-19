693748d97a76084155c3419b10412d0ef2a579fd
yonglinhuang <yonglinhuang@imdada.cn>
2018-04-19 17:41:43 +0800

    [REV] test

--- generators/standard/README.md
+++ generators/standard/README.md
@@ -1,6 +1,18 @@
 # <%- name %>
 <%- description %>
 
+## Install
+```bash
+yarn install
+# if not yarn
+npm install yarn -g
+```
+
+## Start
+```bash
+yarn start
+```
+
 ## 技术栈
 - Vue 2.0
 - Vuex
--- generators/standard/src/main.js
+++ generators/standard/src/main.js
@@ -1,17 +1,17 @@
 import Vue from 'vue'
 import 'es6-promise'
 import 'whatwg-fetch'
-import impression from 'vue-impression'
+import Impression from 'vue-impression'
 import router from './router/'
 import store from './store'
 import App from './app/app'
 
-Vue.use(impression)
+Vue.use(Impression)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#root',
   router,
   store,
-  ...App
+  render: h => h(App)
 })
